7ab45077027a24a8fadeb92e0e8b1ff3f337de59
Cindy Morgan <camorgan70@gmail.com>
2021-01-23 10:54:25 -0500

    commit 3

---
+++ RestaurantRater/Models/Restaurant.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantRater.Models
+{
+    public class Restaurant
+    {
+        [Key]  //sets the property below to be unique identifier
+        public int Id { get; set; }  //primary key
+
+        [Required]  //doesn't allow the property below to be empty
+        public string Name { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        public double Rating { get; set; }
+    }
+}
