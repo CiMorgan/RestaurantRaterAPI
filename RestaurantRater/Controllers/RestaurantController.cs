582901e2be706765ada90d3b707f6c5ef4b9b599
Cindy Morgan <camorgan70@gmail.com>
2021-01-24 19:57:28 -0500

    commit 5

--- RestaurantRater/Controllers/RestaurantController.cs
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -1,6 +1,7 @@
 ﻿using RestaurantRater.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -32,6 +33,78 @@ namespace RestaurantRater.Controllers
 
             //The model is not valid, reject model
             return BadRequest(ModelState);   //ModelState will return what is wrong
+        }
+        // GET ALL - getting all restaurants
+        // api/Restaurant
+        [HttpGet]       
+        public async Task<IHttpActionResult> GetAll()
+        {
+            List<Restaurant> restaurants = await _context.Restaurants.ToListAsync();
+            return Ok(restaurants);
+        }
+
+        // Get By ID
+        // api/Restaurant/{id}
+        [HttpGet]
+        public async Task<IHttpActionResult> GetById([FromUri] int id)
+        {
+            Restaurant restaurant = await _context.Restaurants.FindAsync(id);
+
+            if (restaurant != null)
+            {
+                return Ok(restaurant);
+            }
+            return NotFound();
+        }
+
+        //PUT (update)
+        //api/Restaurant/{id}
+        [HttpPut]
+        public async Task<IHttpActionResult> UpdateRestaurant([FromUri] int id, [FromBody] Restaurant updatedRestaurant)
+        {
+            //Check the ids if they match
+            if (id != updatedRestaurant?.Id)   // ? checks to see if restaurant is null first. If it is, evaluated as false. if not null, does id check.
+            {
+                return BadRequest("Ids do not match.");  
+            }
+            // Check the modelstate
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            //Find the restaurant in the database
+            Restaurant restaurant = await _context.Restaurants.FindAsync(id);
+
+            //If the restaurant doesn't exist, return message
+            if (restaurant is null)
+                return NotFound();
+
+            //Update the restaurant
+            restaurant.Name = updatedRestaurant.Name;
+            restaurant.Address = updatedRestaurant.Address;
+            restaurant.Rating = updatedRestaurant.Rating;
+
+            //Save changes
+            await _context.SaveChangesAsync();
+            return Ok("The restaurant was update.");
+        }
+
+        // Delete
+        //api/Restaurant/{id}
+        [HttpDelete]
+        public async Task<IHttpActionResult> DeleteRestaurant([FromUri] int id)
+        {
+            Restaurant restaurant = await _context.Restaurants.FindAsync(id);
+            if (restaurant is null)
+                return NotFound();
+
+            _context.Restaurants.Remove(restaurant);
+
+            if(await _context.SaveChangesAsync() == 1)    //returns int of number of changes (delete restaurant is one change
+            {
+                return Ok("The restaurant was deleted.");
+            }
+            return InternalServerError();   //restaurant found but not deleted
         }
     }
 }
